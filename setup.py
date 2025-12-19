9b4f09a7de380178b8d388ec32937bb371af815f
Arun <arunprasathravi0@gmail.com>
2025-12-20 02:43:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="ai_testcase",
-    version="0.1",
+    name="tracegenai",
+    version="0.1.0",
+    description="Agentic AI system for SDLC traceability",
+    author="Arun Prasath",
     packages=find_packages(include=[
         "agents*",
         "tools*",
         "utils*",
         "config*",
-        "ui*"
+        "ui*",
+        "rag*"
     ]),
+    install_requires=[
+        line.strip()
+        for line in open("requirements.txt").readlines()
+        if line.strip() and not line.startswith("#")
+    ],
+    python_requires=">=3.10",
 )
